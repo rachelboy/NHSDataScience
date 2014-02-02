17c17d49bdbd2f4bc207ecc3989d5dc3eccfc374
Jamie Gorson <jgorson@ubuntu.(none)>
2014-02-01 17:55:39 -0800

    put in column numbers

--- initial_ingest.py
+++ initial_ingest.py
@@ -1,7 +1,11 @@
 import pandas
+from os.path import expanduser
 import os
+from config.py import config_initial_ingest
+
 def initial_ingest():
-	#change directory to data directory config.data_directory
+	home = expanduser("~")
+	os.chdir(config.data_directory)
 	df = pandas.read_csv(config.infilename)
-	new_df = df[]#list columns you want
+	new_df = df[2,3,5,6]#list columns you want
 	pandas.new_df.to_csv(config.outfilename)
