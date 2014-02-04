6f83c33527f0ab64049126119c727c42525553d9
rachelboy <rachel.boy@gmail.com>
2014-02-04 14:26:14 -0500

    more examining distribtion of drug expenditures

--- totals.py
+++ totals.py
@@ -2,7 +2,7 @@ import config
 import pandas
 import numpy as np
 import thinkstats as ts
-import thinkplot as tp
+import thinkplot
 
 def sumBy(df,key):
 	return df.groupby(key).aggregate(np.sum)
@@ -28,6 +28,30 @@ if __name__ == "__main__":
 	costs = pandas.read_csv("SummedByDrug.csv").to_dict(outtype='list')
 	pmf = ts.MakePmfFromList(costs[Config.keys['nic']])
 	cdf = pmf.MakeCdf()
-	tp.Cdf(cdf)
-	tp.show()
 
+	thinkplot.SubPlot(2, 3, 1)
+	thinkplot.Pmf(pmf)
+	thinkplot.Config(title='pmf')
+
+	thinkplot.SubPlot(2, 3, 2)
+	scale = thinkplot.Cdf(cdf, xscale='log')
+	thinkplot.Config(title='logx', **scale)
+
+	thinkplot.SubPlot(2, 3, 3)
+	scale = thinkplot.Cdf(cdf, transform='exponential')
+	thinkplot.Config(title='expo', **scale)
+
+	thinkplot.SubPlot(2, 3, 4)
+	xs, ys = ts.NormalProbability(costs[Config.keys['nic']])
+	thinkplot.Plot(xs, ys)
+	thinkplot.Config(title='normal')
+
+	thinkplot.SubPlot(2, 3, 5)
+	scale = thinkplot.Cdf(cdf, transform='pareto')
+	thinkplot.Config(title='pareto', **scale)
+
+	thinkplot.SubPlot(2, 3, 6)
+	scale = thinkplot.Cdf(cdf, transform='weibull')
+	thinkplot.Config(title='weibull', **scale)
+
+	thinkplot.Show()
