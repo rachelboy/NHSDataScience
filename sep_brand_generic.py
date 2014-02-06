d37d95dab14aee64c5f33e486f47283cf562cf77
rachelboy <rachel.boy@gmail.com>
2014-02-05 23:29:47 -0500

    minor fixes, not functional

--- sep_brand_generic.py
+++ sep_brand_generic.py
@@ -2,14 +2,15 @@ import pandas
 import os
 import config
 
+def isGeneric(bnf):
+	end_bnf = bnf[-4:] 
+	return end_bnf[0:2] == end_bnf[2:4]
+
 def sep_bran_generic(Config):
-	print 'in function'
 	Config.config_sep_brand_generic()
 	for infile, outfile_brand, outfile_gen in Config.filenames:
 		try:
-			print 'bringing in file'
 			df = pandas.read_csv(infile)
-			print 'intake file'
 		except:
 			print "file", infile, "not found in", Config.data_directory
 			continue
@@ -18,17 +19,15 @@ def sep_bran_generic(Config):
 			bnf = row[Config.keys['bnf']]
 			end_bnf = bnf[-4:] 
 			if end_bnf[0:2] == end_bnf[2:4]:
-				print 'in if statement'
 				try:
-					generic.append(row)
-				except:
-				 	generic = Dataframe(row)
+					generic = generic.append(row)
+				except NameError:
+				 	generic = pandas.DataFrame(row)
 			else:
-				print 'in else statement'
 				try:
-					brand.append(row)
-				except:
-				 	brand = Dataframe(row)
+					brand = brand.append(row)
+				except NameError:
+				 	brand = pandas.DataFrame(row)
 		print 'to csv'
 		generic.to_csv(outfile_gen,index=False)
 		brand.to_csv(outfile_brand,index=False)
