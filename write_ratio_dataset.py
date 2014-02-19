4fe5314392ec4206738f83711f7593dbe1eab30a
rachelboy <rachel.boy@gmail.com>
2014-02-19 15:39:43 -0500

    debugged and working

--- write_ratio_dataset.py
+++ write_ratio_dataset.py
@@ -1,18 +1,62 @@
 import config
+import util
+import pandas
+from os.path import expanduser
+import os
+
+class Pipeline(object):
+	def __init__(self,Config):
+		self.Config = Config
+
+
+	def loadDF(self,filename):
+		print "Loading", filename
+		try:
+			return pandas.read_csv(filename, index_col = False)
+		except:
+			print "trouble loading", filename, "in", self.Config.data_directory
+			return False
 class Write_ratio_dataset(Pipeline):
 	def run(self):
 		bfile = self.Config.append_dir("WriteRatioDatasetBrand")
 		gfile = self.Config.append_dir("WriteRatioDatasetGeneric")
+		ofile = self.Config.append_dir("WriteRatioDatasetOut")
 
-		output = []
-		for (brand, generic, month) in zip(bfile,gfile,self.Config.filenames):
+		for (brand, generic, outfile) in zip(bfile,gfile,ofile):
 			brandfile = self.loadDF(brand)
 			genericfile = self.loadDF(generic)
-			brandnum = len(brandfile.index)
-			genericnum = len(genericfile.index)
-			ratio = brandnum/genericnum
-			output.append(month,ratio)
-		output.to_csv('Generic_Brand_Ratios')
+			
+			groupedbrand = util.sumBy(brandfile,self.Config.keys['practice'])
+			groupedgeneric = util.sumBy(genericfile,self.Config.keys['practice'])
+			
+			output = pandas.DataFrame.merge(
+				groupedbrand, 
+				groupedgeneric, 
+				on=self.Config.keys['practice'],
+				how = 'outer', 
+				suffixes =('brand','generic'))
+			
+
+			items = self.Config.keys['items']
+			quantity = self.Config.keys['quantity']
+			nic = self.Config.keys['nic']
+			output['ratioitems']= output[items+'brand']/output[items+'generic']
+			output['ratioquantity'] = output[quantity+'brand']/output[quantity+'generic']
+			output['rationic'] = output[nic+'brand']/output[nic+'generic']
+			
+
+			output = output.drop(items+'brand', axis = 1)
+			output = output.drop(items+'generic',axis = 1)
+			output = output.drop(quantity+'brand', axis = 1)
+			output = output.drop(quantity+'generic', axis = 1)
+			output = output.drop(nic+'brand', axis = 1)
+			output = output.drop(nic+'generic', axis = 1)
+			output = output.drop('INCLUDE'+'generic', axis = 1)
+			output = output.drop('GENERIC'+'generic', axis = 1)
+			output = output.drop('INCLUDE'+'brand', axis = 1)
+			output = output.drop('GENERIC'+'brand', axis = 1)
+
+			output.to_csv(outfile, index = False)
 
 
 
@@ -20,4 +64,5 @@ class Write_ratio_dataset(Pipeline):
 
 if __name__ == "__main__":
 	Config = config.Config()
-	write_ratio_dataset.py(Config)
+	next = Write_ratio_dataset(Config)
+	next.run()
