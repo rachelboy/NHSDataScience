c440a4b134bb21c8a08ad49284f56cf24209de51
Jamie Gorson <jgorson@ubuntu.(none)>
2014-03-06 21:48:06 -0500

    added nsaid

--- config.py
+++ config.py
@@ -5,20 +5,22 @@ class Config(object):
 
 	def __init__(self, changeDir=True):
 		#directory containing all the data files
-		self.data_directory = "/media/rboy/22A7-AB54"
+		self.data_directory = "/media/jgorson/22A7-AB54"
 		if changeDir:
 			os.chdir(self.data_directory)
-		self.keys = {'practice':'PRACTICE','bnf':'BNF CODE',
+		self.keys = {'practice':'PRACTICE','pct': 'PCT','bnf':'BNF CODE',
 		                 'items':'ITEMS  ','nic':'NIC        ',
 		                 'quantity':'QUANTITY', 'post code':'postal code',
 		                 'gen':'GENERIC'}
-		self.filenames = ['Oct2013.csv', 'Sep2013.csv', 'Aug2013.csv', 
-					'Jul2013.csv', 'Jun2013.csv', 'May2013.csv', 'Apr2013.csv', 
-					'Mar2013.csv', 'Feb2013.csv', 'Jan2013.csv', 'Dec2012.csv', 
-					'Nov2012.csv', 'Oct2012.csv', 'Sep2012.csv', 'Aug2012.csv', 
-					'Jul2012.csv','Jun2012.csv', 'May2012.csv', 'Apr2012.csv', 
-					'Mar2012.csv', 'Feb2012.csv', 'Jan2012.csv']
+		self.filenames = ['tiny.csv']#['Oct2013.csv', 'Sep2013.csv', 'Aug2013.csv', 
+					#'Jul2013.csv', 'Jun2013.csv', 'May2013.csv', 'Apr2013.csv', 
+					#'Mar2013.csv', 'Feb2013.csv', 'Jan2013.csv', 'Dec2012.csv', 
+					#'Nov2012.csv', 'Oct2012.csv', 'Sep2012.csv', 'Aug2012.csv', 
+					#'Jul2012.csv','Jun2012.csv', 'May2012.csv', 'Apr2012.csv', 
+					#'Mar2012.csv', 'Feb2012.csv', 'Jan2012.csv']
 		self.ppis_file = 'Criteria/ppis.csv'
+		self.naproxen_file = 'NSAID/naproxen.csv'
+		self.diclofenac_file = 'NSAID/diclofenac.csv'
 		self.directories ={'Ingest_in': 'RawData', 'Ingest_out': 'CompressedData',
 							'Join_ppis_in':'CompressedData', 'Join_ppis_out':'JoinedPpis',
 							'Join_post_codes_in': 'JoinedPpis', 'Addresses':'Addresses', 'Join_post_codes_out': 'JoinedAdds',
@@ -29,9 +31,13 @@ class Config(object):
 							'MakeDrugPairsIn': 'JoinedAdds', 'MakeDrugPairsOut': 'PpiDrugPairings',
 							'OutCodeDrugsIn': 'PpiDrugPairings', 'OutCodeDrugsOut': 'OutCodePpiPairings',
 							'CorrsIn': ['JoinedPpis','SepBrand','SepGeneric']}
-	def append_dir(self, directory):
-		return [self.directories.get(directory,directory) + '/' +  item for item in self.filenames]
-	
+		self.nsaid_directories = {'Ingest_in' : 'test', 'Ingest_out_nap' : 'NapCompressedData', 'Ingest_out_dic' : 'DicCompressedData'}
+	def append_dir(self, directory, group = 'PPIS'):
+		if group == 'PPIS':
+			return [self.directories.get(directory,directory) + '/' +  item for item in self.filenames]
+		if group == 'NSAID':
+			return [self.nsaid_directories.get(directory,directory) + '/' +  item for item in self.filenames]
+		
 
 class TestConfig(Config):
 
